f03c3f8295e7f88edb53ac5ef171e4962ff3a12f
Abram Shook <ajshook77@gmail.com>
2022-05-02 08:34:21 -0500

    commit tree 4fdfe919427ad9d90101b0051d60cc8c82ba1cda

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  function destructivelyAppendCat(name)
+  {cats.push('Ralph');
+}
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  function destructivelyPrependCat(name)
+  {cats.unshift('Bob')}
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  function destructivelyRemoveLastCat()
+  {cats.pop()}
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  function destructivelyRemoveFirstCat()
+  {cats.shift()}
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  function appendCat(name) {
+const copyOfCats = [...cats, name]
+return copyOfCats
+  }
+  function prependCat(name) {
+ const copyOfCats = ['Arnold', ...cats]
+return copyOfCats }
+function removeLastCat() {
+const removeLastCat = cats.slice(0, 2)
+    return removeLastCat
+}
+function removeFirstCat() {
+    const removeFirstCat = cats.slice(1)
+    return removeFirstCat    
+}
